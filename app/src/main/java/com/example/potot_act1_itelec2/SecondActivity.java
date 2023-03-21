e107bd913327f7d2b40d78b56561c288e57a3fb3
Catherinejc <tapcoalcoriza17@gmail.com>
2023-03-21 23:15:16 +0800

    Recent

---
+++ app/src/main/java/com/example/potot_act1_itelec2/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.example.potot_act1_itelec2;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button Btn = (Button) findViewById(R.id.Button3);
+
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);
+                
+            }
+        });
+    }
+}
